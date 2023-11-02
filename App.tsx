f5d9016039a40c1e93ee42c5ab806b9bbb27d228
vaheed <vaheedsk36@gmail.com>
2023-11-03 01:56:10 +0530

    init-files

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+function App(): JSX.Element {
+
+  return (
+     <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
+
+export default App;
